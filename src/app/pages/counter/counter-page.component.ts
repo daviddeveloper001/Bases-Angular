99f670db21a6ac3ffdc328d494a7d8ae10c962ac
daviddeveloper001 <deivydevolper@gmail.com>
2025-10-27 14:08:04 -0500

    fix:ChangeDetectionStrategy

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,13 +1,21 @@
-import { signal } from '@angular/core';
+import { ChangeDetectionStrategy, signal } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
     templateUrl: './counter-page.component.html',
     styleUrls: ['./counter-page.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
     counter = 15;
     counterSignal = signal(10);
+
+    constructor(){
+      setInterval(() => {
+        this.counterSignal.update((v) => v + 1);
+        console.log('tick');
+      }, 2000)
+    }
 
     increaseBy(value: number): void {
         this.counter += value;
